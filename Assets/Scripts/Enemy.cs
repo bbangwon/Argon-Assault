562d6cc678b84486c432a1a48d5794e9958e1f36
youngil <youngil@placeb.com>
2024-03-06 15:48:59 +0900

    적군 명중하기

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,11 +7,17 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     GameObject deathVFX;
 
+    [SerializeField] 
+    GameObject hitVFX;
+
     [SerializeField]
     Transform parent;
 
     [SerializeField]
     int scorePerHit = 15;
+
+    [SerializeField]
+    int hitPoints = 4;
 
     ScoreBoard scoreBoard;
 
@@ -23,10 +29,16 @@ public class Enemy : MonoBehaviour
     void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        KillEnemy();
+        if(hitPoints < 1)
+        {
+            KillEnemy();
+        }
     }
     void ProcessHit()
     {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        hitPoints--;
         scoreBoard.IncreaseScore(scorePerHit);
     }
 
