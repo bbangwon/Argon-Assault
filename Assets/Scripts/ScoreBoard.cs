bb3a125e2fcd05e486842eb709908786518a18dc
youngil <youngil@placeb.com>
2024-03-06 09:29:08 +0900

    스코어 표시를 위한 ToString() 함수

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     int score;
+    TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        scoreText = GetComponent<TextMeshProUGUI>();
+        scoreText.text = "Start";
+    }
 
     public void IncreaseScore(int amountToIncrease)
     {
         score += amountToIncrease;
-        Debug.Log("Score: " + score);
+        scoreText.text = score.ToString();
     }
-
-
 }
